f736cfe30ea9e18206c184ef39de2cb36937af25
DARIOALVARENGA <dalvarenga@rfelino.ar>
2026-06-11 11:09:27 -0300

    Update and rename api - proxy.js to api/proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,9 +1,17 @@
+export const config = {
+  api: {
+    bodyParser: {
+      sizeLimit: '1mb',
+    },
+  },
+};
+
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', '*');
   res.setHeader('Access-Control-Max-Age', '86400');
-  
+
   if (req.method === 'OPTIONS') {
     res.status(200).end();
     return;
